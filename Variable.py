4684aa3967ff477f3afc7be217a8c041ceff6aee
Lohith Kumar B N <lohithkumar88617@gmail.com>
2026-02-03 11:35:03 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,15 @@
+student_name = "Lohith Kumar"
+age = 21
+course = "MCA"
+
+print("=" * 40)
+print("      STUDENT INFORMATION SYSTEM")
+print("=" * 40)
+
+print("Name   :", student_name)
+print("Age    :", age)
+print("Course :", course)
+
+print("=" * 40)
+print("Program Completed Successfully!")
+print("=" * 40)
