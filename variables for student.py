6fdd84240a1f60cf342d65c9866011c2a4d35992
Lohith Kumar B N <lohithkumar88617@gmail.com>
2026-02-03 11:25:23 +0530

    Add files via upload

---
+++ variables for student.py
@@ -0,0 +1,21 @@
+print("=" * 45)
+print("STUDENT INFORMATION SYSTEM")
+print("=" * 45)
+
+student_name = input("Enter Student Name : ")
+
+age = int(input("Enter Age : "))
+
+course = input("Enter Course : ")
+
+print("\n" + "=" * 45)
+print("STUDENT DETAILS")
+print("=" * 45)
+
+print(f"Name   : {student_name}")
+print(f"Age    : {age}")
+print(f"Course : {course}")
+
+print("=" * 45)
+print(" Data Stored Successfully!")
+print("=" * 45)
